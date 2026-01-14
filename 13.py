b6522608bb8f0bf9b8c02ab57c96c2745dadbd76
Alok Kumar Nayak <alokkumar.nayak2009@gmail.com>
2026-01-14 09:06:17 +0530

    14-01-2026

---
+++ 13.py
@@ -0,0 +1,25 @@
+# a = 10
+# b = 15
+# c = -100
+# print(type(a)) # <class 'int'>
+# print(type(b)) # <class 'int'>
+# print(type(c)) #<class 'int'>
+
+
+# a = 10.0
+# b = 15.12
+# c = -100.0
+# print(type(a)) # <class 'float'>
+# print(type(b)) # <class 'float'>
+# print(type(c)) #<class 'float'>
+
+
+a = 1+10j
+b = -2-5j
+print(type(a)) #<class 'complex'>
+print(type(b)) #<class 'complex'>
+
+print(a.real)  # 1.0
+print(a.imag)  # 10.0
+print(b.real)  # -2.0
+print(b.imag)  # -5.0
