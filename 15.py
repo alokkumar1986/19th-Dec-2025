ce54b0a792d1865465033b06c9cd934e7de0fe8b
Alok Kumar Nayak <alokkumar.nayak2009@gmail.com>
2026-01-19 09:17:01 +0530

    '19-01-2026'

---
+++ 15.py
@@ -0,0 +1,17 @@
+'''
+a = 10
+b = float(a)
+print(type(a))
+print(type(b))
+'''
+'''
+a = 12.5
+b = int(a)
+print(type(a))
+print(type(b))
+'''
+
+a = 10
+b = complex(a)
+print(type(a))
+print(type(b))
---
+++ 16.py
@@ -0,0 +1,62 @@
+'''a = 'aptech'
+
+b = "APTECH"
+
+print("Aptech")
+
+print(type(a)) #<class 'str'>
+print(type(b)) #<class 'str'>
+'''
+
+'''
+a = 'Ram\'s House'
+b = "Aliva's brother"
+print(a)
+print(b)
+'''
+
+# a = "We are Learning Python Programming. 
+# It is a part of Data Science Course"
+
+
+# a = '''We are Learning Python Programming. 
+# It is a part of Data Science Course.'''
+
+# b = """We are Learning Python Programming. 
+# It is a part of Data Science Course."""
+
+# print(a)
+# print(b)
+
+'''
+a = "Apte\bch"
+print(a)
+
+b = "Apte\rch"
+print(b)  #chte
+
+c = "Learning\rPython"
+print(c)
+
+d = "Ap\ttech"
+print(d)  #Ap tech
+'''
+
+'''a = "Aptech"
+b = "Learning"
+
+print(len(a))
+print(len(b))
+'''
+
+# a = "Aptech Learning"
+# b = 'tech'
+
+# print(b in a)   #True
+# print(a in b)   #False
+
+
+a = "Aptech Learning"
+
+for i in a:
+ print(i, end="")
